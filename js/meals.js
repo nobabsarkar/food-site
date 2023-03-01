72637b8188e20cfbd2d1ebd6b07cc8aff1318599
Nobab <nobabsarkar2016@gmail.com>
2023-03-01 07:29:03 +0600

    first commit

---
+++ js/meals.js
@@ -0,0 +1,62 @@
+const loadMeals = (search) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMeals(data.meals));
+};
+
+const displayMeals = (meals) => {
+  const mealsContainer = document.getElementById("meal-container");
+  mealsContainer.innerHTML = "";
+  meals.forEach((meal) => {
+    // console.log(meal);
+    const mealDiv = document.createElement("div");
+    mealDiv.classList.add("col");
+    mealDiv.innerHTML = `
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card">
+              <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">
+                 ${meal.strInstructions.slice(0, 200)}
+                </p>
+            </div>
+        </div>
+      `;
+    mealsContainer.appendChild(mealDiv);
+  });
+};
+
+const searchFood = () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  searchField.value = "";
+  loadMeals(searchText);
+};
+
+const loadMealDetail = (meal) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${meal}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayMealDetail(data.meals[0]));
+};
+
+const displayMealDetail = (meal) => {
+  const detailContainer = document.getElementById("detail-container");
+  detailContainer.innerHTML = "";
+  const mealDiv = document.createElement("div");
+  mealDiv.classList.add("card");
+  mealDiv.innerHTML = `
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
+            <div class="card-body">
+              <h5 class="card-title">${meal.strMeal}</h5>
+              <p class="card-text">
+              ${meal.strInstructions.slice(0, 200)}
+              </p>
+            
+            </div>
+    `;
+  detailContainer.appendChild(mealDiv);
+};
+
+loadMeals("");
